5a162cf24a5249b024979ed57d53c17fa4475409
Lucas <lucasnkz11@gmail.com>
2022-01-30 11:50:39 -0300

    layout e js de selecao pronto

--- script.js
+++ script.js
@@ -1,14 +1,38 @@
 let selecionado;
+let comida = null;
+let refrigerante = null;
+let sobremesa = null;
 
     function selecionarPrato(seletor){
-        const selecionado = document.querySelector(".selecionado")
+        const selecionado = document.querySelector(".pratos .selecionado")
         if (selecionado !== null) {
             selecionado.classList.remove("selecionado")
         }
         const addSelecionado = document.querySelector("."+seletor)
         addSelecionado.classList.add("selecionado")
     }
+    function selecionarBebida(seletor){
+        const selecionado = document.querySelector(".bebidas .selecionado")
+        if (selecionado !== null) {
+            selecionado.classList.remove("selecionado")
+        }
+        const addSelecionado = document.querySelector("."+seletor)
+        addSelecionado.classList.add("selecionado")
+    }
+    function selecionarSobremesa(seletor){
+        const selecionado = document.querySelector(".sobremesas .selecionado")
+        if (selecionado !== null) {
+            selecionado.classList.remove("selecionado")
+        }
+        const addSelecionado = document.querySelector("."+seletor)
+        addSelecionado.classList.add("selecionado")
+    }
+    function verificaSelecao(){
+        
+        if ((comida !== null) && (refrigerante !== null) && (sobremesa !== null)){
 
+        }
+    }
 
 
 // CRIA UMA FUNCAO PARA LIMPAR OS DADOS
@@ -20,4 +44,3 @@ let selecionado;
 
 // CRIA UMA FUNCAO ---- VERIFICA SE OS REQUISITOS FORAM CUMPRIDOS
 // verifica se todos os requisitos são true ---- se todos forem true libera o botao para finalizar
-// 
